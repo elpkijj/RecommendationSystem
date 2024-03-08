3bc63b9ed71df93f7c873423139fb7258451fb89
ljl <liu522918700@163.com>
2024-03-09 00:29:33 +0800

    增加城市名

--- KG/kg_query.py
+++ KG/kg_query.py
@@ -2,23 +2,26 @@ from py2neo import Graph
 
 graph = Graph("http://localhost:7474", auth=("neo4j", "XzJEunfiT2G.t2Y"), name="neo4j")
 
-def query_knowledge_graph(id):
+def query_knowledge_graph(identifier):
     query = f"""
-    MATCH (i:Identity {{name: '{id}'}})-[:HAS_JOB]->(j:Job)
+    MATCH (i:Identity {{name: '{identifier}'}})
+    MATCH (i)-[:HAS_JOB]->(j:Job)
     MATCH (i)-[:HAS_ADDRESS]->(a:Address)
     MATCH (i)-[:HAS_SALARY]->(s:Salary)
     MATCH (i)-[:CONTAINS]->(k:Keyword)
-    RETURN j.title AS Job_Title, a.name AS Address, s.name AS Salary_Range, COLLECT(k.name) AS Keywords, i.repsonsibility AS Responsibility
-
+    OPTIONAL MATCH (i)-[:LOCATED_IN]->(c:City)
+    RETURN j.title AS Job_Title, a.name AS Address, s.name AS Salary_Range, COLLECT(k.name) AS Keywords, i.repsonsibility AS Responsibility, c.name AS City
     """
 
     result = graph.run(query).data()
     return result
 
 if __name__ == "__main__":
-    id = input("请输入ID：")
-    result = query_knowledge_graph(id)
+    identifier = input("请输入ID：")
+    result = query_knowledge_graph(identifier)
 
-    for item in result:
+    if result:
+        for item in result:
             print(item)
-
+    else:
+        print(f"No information found for the Identity/Keyword: {identifier}")
