982bfec432c9829f953dd86d429f4170187c2cac
ljl <liu522918700@163.com>
2024-03-10 22:23:25 +0800

    生成企业招聘信息的json文件

--- KG/kg_query.py
+++ KG/kg_query.py
@@ -1,27 +1,27 @@
 from py2neo import Graph
+import json
 
 graph = Graph("http://localhost:7474", auth=("neo4j", "XzJEunfiT2G.t2Y"), name="neo4j")
 
-def query_knowledge_graph(identifier):
-    query = f"""
-    MATCH (i:Identity {{name: '{identifier}'}})
-    MATCH (i)-[:HAS_JOB]->(j:Job)
-    MATCH (i)-[:HAS_ADDRESS]->(a:Address)
-    MATCH (i)-[:HAS_SALARY]->(s:Salary)
-    MATCH (i)-[:CONTAINS]->(k:Keyword)
+def query_knowledge_graph():
+    query = """
+    MATCH (i:Identity)
+    MATCH (i)-[:REQUIRES]->(e:EducationRequirement)
+    OPTIONAL MATCH (i)-[:HAS_ADDRESS]->(a:Address)
+    OPTIONAL MATCH (i)-[:HAS_SALARY]->(s:Salary)
+    OPTIONAL MATCH (i)-[:CONTAINS]->(k:Keyword)
     OPTIONAL MATCH (i)-[:LOCATED_IN]->(c:City)
-    RETURN j.title AS Job_Title, a.name AS Address, s.name AS Salary_Range, COLLECT(k.name) AS Keywords, i.repsonsibility AS Responsibility, c.name AS City
+    RETURN i.name AS Identity, e.name AS Education_Requirement, a.name AS Address, s.name AS Salary_Range, COLLECT(k.name) AS Keywords, i.repsonsibility AS Responsibility, c.name AS City
     """
 
     result = graph.run(query).data()
     return result
 
-if __name__ == "__main__":
-    identifier = input("请输入ID：")
-    result = query_knowledge_graph(identifier)
+def store_results_to_json():
+    all_results = query_knowledge_graph()
 
-    if result:
-        for item in result:
-            print(item)
-    else:
-        print(f"No information found for the Identity/Keyword: {identifier}")
+    with open("all_info.json", "w", encoding='utf-8') as file:
+        json.dump(all_results, file, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    store_results_to_json()
