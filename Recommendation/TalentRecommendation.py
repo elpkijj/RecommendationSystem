49a745acf5d4ef0ae2e7195988ba810e3ba8cab3
DIONG666 <8208220511@csu.edu.cn>
2024-04-15 15:48:05 +0800

    2.0.1

--- Recommendation/TalentRecommendation.py
+++ Recommendation/TalentRecommendation.py
@@ -15,7 +15,7 @@ def get_db_connection():
 
 @talents.route('/talents/recommended/<int:user_id>', methods=['GET'])
 def get_recommended_talents(user_id):
-    time.sleep(2)
+    time.sleep(3)
     conn = get_db_connection()
     cursor = conn.cursor()
     # 查询推荐人才ID和匹配度
